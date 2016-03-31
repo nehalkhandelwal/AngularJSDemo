05456d8a7a2398dafdf70d38b76eda75639e7701
nehalkhandelwal <nehalkhandelwal8@gmail.com>
2016-03-31 11:11:22 +0530

    Initial commit

---
+++ myApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by nehalkhandelwal on 14/03/16.
+ */
+var app = angular.module("myApp",[]);
---
+++ myCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by nehalkhandelwal on 14/03/16.
+ */
+app.controller("myCntrl",function($scope){
+    $scope.firstName= "Nehal";
+    $scope.lastName= "Khandelwal";
+});
